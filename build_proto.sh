3a1baf8bbc276a24fb84ee822627915533f4bcd5
Andrew Spiering <aspiering@zynga.com>
2018-01-12 00:28:42 -0800

    Fix
    - Fixing an issue with Non-Windows build_proto.sh

--- build_proto.sh
+++ build_proto.sh
@@ -3,6 +3,6 @@
 protoc --proto_path=./proto --cpp_out=./plugin/src/lib/generated ./proto/event_plugin.proto
 protoc --proto_path=./proto --plugin=protoc-gen-zsharp=./plugin/bazel-bin/protoc-gen-zsharp --zsharp_out=./runtime/src/Zynga.Protobuf.Runtime/Generated ./proto/event_plugin.proto
 protoc --proto_path=./proto --plugin=protoc-gen-zsharp=./plugin/bazel-bin/protoc-gen-zsharp --zsharp_out=./runtime/src/Zynga.Protobuf.Runtime/Generated ./proto/event_source.proto
-protoc --proto_path=./proto/ --plugin=protoc-gen-zsharp=./plugin/bazel-bin/protoc-gen-zsharp --zsharp_out=--zsharp_out=checksum=true:./runtime/src/Zynga.Protobuf.Runtime.Tests ./proto/event_test.proto
+protoc --proto_path=./proto/ --plugin=protoc-gen-zsharp=./plugin/bazel-bin/protoc-gen-zsharp --zsharp_out=checksum=true:./runtime/src/Zynga.Protobuf.Runtime.Tests ./proto/event_test.proto
 
 

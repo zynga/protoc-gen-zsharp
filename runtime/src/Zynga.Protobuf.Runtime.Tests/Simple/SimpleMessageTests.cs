07a8741c481712ba7c9b1f4d4bca78c50f908183
Joshua Garnett <jgarnett@zynga.com>
2018-03-06 21:33:16 -0500

    Created test case for issue with setting nested messages

--- runtime/src/Zynga.Protobuf.Runtime.Tests/Simple/SimpleMessageTests.cs
+++ runtime/src/Zynga.Protobuf.Runtime.Tests/Simple/SimpleMessageTests.cs
@@ -11,12 +11,30 @@ namespace Zynga.Protobuf.Runtime.Tests.Simple {
 			parent.B = childA;
 			var childB = new ChildMessage();
 			parent.B = childB;
-			
+
 			parent.ClearEvents();
 
 			// childA changes shouldn't generate events on the parent anymore
 			childA.C = 100;
 			AssertNotGenerated(parent);
+		}
+
+		[Fact]
+		public void ShouldGenerateEventsWithObjectIntializer() {
+			var a = new ChildMessage {D = new ChildChildMessage()};
+
+			var b = new ChildMessage {
+				D = new ChildChildMessage()
+			};
+
+			var parent = new ParentMessage {
+				B = b
+			};
+			AssertGenerated(parent);
+			parent.ClearEvents();
+
+			parent.B.D.E = 10;
+			AssertDeltaPath(parent, new int[]{2, 4, 5});
 		}
 	}
 }
